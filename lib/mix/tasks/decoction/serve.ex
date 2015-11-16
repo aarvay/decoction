373255acf5b10edded91da4776aaf0c25477a299
Vignesh Rajagopalan <me@aarvay.com>
2015-11-17 01:49:06 +0530

    Wrap a long line of code

--- lib/mix/tasks/decoction/serve.ex
+++ lib/mix/tasks/decoction/serve.ex
@@ -14,7 +14,9 @@ defmodule Mix.Tasks.Decoction.Serve do
   def run(_) do
     :application.start :inets
     server_root = '#{Path.expand("../../_site", Mix.Project.build_path)}'
-    mime_types = [{'html', 'text/html'},{'htm','text/html'},{'js','text/javascript'},{'css','text/css'},{'gif','image/gif'},{'jpg','image/jpeg'},{'jpeg','image/jpeg'},{'png','image/png'}]
+    mime_types = [{'html', 'text/html'},{'htm','text/html'},{'js','text/javascript'},
+                  {'css','text/css'},{'gif','image/gif'},{'jpg','image/jpeg'},
+                  {'jpeg','image/jpeg'},{'png','image/png'}]
 
     local_server_config = [
       port: 4000,
